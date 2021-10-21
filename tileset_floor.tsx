4d72d32d8c49d6d9bd9f3e2545a86265e2139bf4
coderwelsch <social@coderwelsch.com>
2021-10-21 17:56:00 +0200

    Removed redundant files

--- tileset_floor.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="tileset_floor" tilewidth="32" tileheight="32" tilecount="4096" columns="64">
- <image source="tileset_floor.png" width="2048" height="2048"/>
-</tileset>
