6f233961726c3ed5e64262c921eba88fb85d7117
coderwelsch <social@coderwelsch.com>
2021-10-29 14:05:03 +0200

    Removed cccplogo.tsx

--- cccplogo.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.2" name="cccplogo" tilewidth="32" tileheight="32" tilecount="2" columns="2">
- <image source="cccplogo.png" width="64" height="32"/>
-</tileset>
