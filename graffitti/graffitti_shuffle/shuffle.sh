aa9376c956813103111482a8ba0804ff3201af6f
Christoph Sterz <christoph.sterz@kdab.com>
2021-12-13 14:15:51 +0100

    Graffitti: Made Grafitti continous

--- graffitti/graffitti_shuffle/shuffle.sh
+++ graffitti/graffitti_shuffle/shuffle.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 rm shuffled_graffiti.png
 ls *.png | shuf > imagelist.txt
-montage @imagelist.txt -background none -tile 1x -geometry x64-1+0 -gravity west shuffled_graffiti.png
-
+montage @imagelist.txt -background none -tile x10  -geometry +0+0 shuffled_graffiti.png
+#-geometry x64+0+0 -gravity west
