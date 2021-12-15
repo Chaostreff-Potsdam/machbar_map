d8c827d34e90e3846413b99c9ba5596beb9a32d2
Christoph Sterz <christoph.sterz@kdab.com>
2021-12-15 22:04:12 +0100

    Graffiti shuffle

--- graffitti/graffitti_shuffle/shuffle.sh
+++ graffitti/graffitti_shuffle/shuffle.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/bash
-rm shuffled_graffiti.png
+rm -f shuffled_graffiti.png
+rm -f output.png
 ls *.png | shuf > imagelist.txt
 montage @imagelist.txt -background none -tile x10  -geometry +0+0 shuffled_graffiti.png
-#-geometry x64+0+0 -gravity west
+convert shuffled_graffiti.png -background none -gravity west -extent 2048x640 output.png
